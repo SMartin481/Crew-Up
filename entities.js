cdb5cede724286d867a5a20694514bd912139a9f
SMartin481 <69651524+SMartin481@users.noreply.github.com>
2025-10-03 18:36:22 -0500

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,355 @@
+import { base44 } from './base44Client';
+
+
+export const CrewProfile = base44.entities.CrewProfile;
+
+export const CompanyProfile = base44.entities.CompanyProfile;
+
+export const ServiceRequest = base44.entities.ServiceRequest;
+
+export const Contract = base44.entities.Contract;
+
+export const Invoice = base44.entities.Invoice;
+
+export const Transaction = base44.entities.Transaction;
+
+export const FloatLedger = base44.entities.FloatLedger;
+
+export const Message = base44.entities.Message;
+
+export const Credit = base44.entities.Credit;
+
+export const Production = base44.entities.Production;
+
+export const CrewRequest = base44.entities.CrewRequest;
+
+export const RateNegotiation = base44.entities.RateNegotiation;
+
+export const RateBaseline = base44.entities.RateBaseline;
+
+export const Person = base44.entities.Person;
+
+export const Film = base44.entities.Film;
+
+export const TelevisionSeries = base44.entities.TelevisionSeries;
+
+export const Notification = base44.entities.Notification;
+
+export const DocumentUpload = base44.entities.DocumentUpload;
+
+export const ProjectRecommendation = base44.entities.ProjectRecommendation;
+
+export const PerformanceRating = base44.entities.PerformanceRating;
+
+export const MarketAnalytics = base44.entities.MarketAnalytics;
+
+export const ProjectBudgetTracking = base44.entities.ProjectBudgetTracking;
+
+export const InvestmentProposal = base44.entities.InvestmentProposal;
+
+export const Conversation = base44.entities.Conversation;
+
+export const ProductionPack = base44.entities.ProductionPack;
+
+export const Booking = base44.entities.Booking;
+
+export const RateProposal = base44.entities.RateProposal;
+
+export const ContractTemplate = base44.entities.ContractTemplate;
+
+export const ContractClause = base44.entities.ContractClause;
+
+export const AdminAuditLog = base44.entities.AdminAuditLog;
+
+export const Training = base44.entities.Training;
+
+export const Company = base44.entities.Company;
+
+export const CrewSourcingAgreement = base44.entities.CrewSourcingAgreement;
+
+export const CoordinatorBrief = base44.entities.CoordinatorBrief;
+
+export const ContactAccessRequest = base44.entities.ContactAccessRequest;
+
+export const OnboardingTask = base44.entities.OnboardingTask;
+
+export const DigitalSignature = base44.entities.DigitalSignature;
+
+export const VendorProfile = base44.entities.VendorProfile;
+
+export const VendorCompanyPermission = base44.entities.VendorCompanyPermission;
+
+export const GovernmentGoal = base44.entities.GovernmentGoal;
+
+export const WorkforceMetric = base44.entities.WorkforceMetric;
+
+export const CostIndex = base44.entities.CostIndex;
+
+export const SupplyDemandMetric = base44.entities.SupplyDemandMetric;
+
+export const GovernmentBenchmark = base44.entities.GovernmentBenchmark;
+
+export const BenchmarkComparison = base44.entities.BenchmarkComparison;
+
+export const GeneratedReport = base44.entities.GeneratedReport;
+
+export const MessageThread = base44.entities.MessageThread;
+
+export const MessageRecord = base44.entities.MessageRecord;
+
+export const MessageHeader = base44.entities.MessageHeader;
+
+export const MessageAudit = base44.entities.MessageAudit;
+
+export const MessageLabel = base44.entities.MessageLabel;
+
+export const ArchiveBucket = base44.entities.ArchiveBucket;
+
+export const MessageExport = base44.entities.MessageExport;
+
+export const Timesheet = base44.entities.Timesheet;
+
+export const TimesheetEntry = base44.entities.TimesheetEntry;
+
+export const TimesheetCompanyDecision = base44.entities.TimesheetCompanyDecision;
+
+export const TimesheetChangeRequest = base44.entities.TimesheetChangeRequest;
+
+export const OnboardingGate = base44.entities.OnboardingGate;
+
+export const TimesheetRedFlag = base44.entities.TimesheetRedFlag;
+
+export const TimesheetAuditLog = base44.entities.TimesheetAuditLog;
+
+export const ProjectCast = base44.entities.ProjectCast;
+
+export const ProjectCrew = base44.entities.ProjectCrew;
+
+export const ProjectCompleteness = base44.entities.ProjectCompleteness;
+
+export const IncentiveDiscount = base44.entities.IncentiveDiscount;
+
+export const DataVerificationRequest = base44.entities.DataVerificationRequest;
+
+export const PromoCode = base44.entities.PromoCode;
+
+export const TrainingDocument = base44.entities.TrainingDocument;
+
+export const TrainingDocumentVersion = base44.entities.TrainingDocumentVersion;
+
+export const TrainingAuditLog = base44.entities.TrainingAuditLog;
+
+export const PreparedUser = base44.entities.PreparedUser;
+
+export const ImportBatch = base44.entities.ImportBatch;
+
+export const ImportLeadRaw = base44.entities.ImportLeadRaw;
+
+export const ImportLeadNormalized = base44.entities.ImportLeadNormalized;
+
+export const RoleMapping = base44.entities.RoleMapping;
+
+export const StagedUser = base44.entities.StagedUser;
+
+export const ImportAnalyticsFact = base44.entities.ImportAnalyticsFact;
+
+export const CallLog = base44.entities.CallLog;
+
+export const RateAuditLog = base44.entities.RateAuditLog;
+
+export const NDA = base44.entities.NDA;
+
+export const RebateJurisdiction = base44.entities.RebateJurisdiction;
+
+export const AuditBinder = base44.entities.AuditBinder;
+
+export const AuditBinderItem = base44.entities.AuditBinderItem;
+
+export const AuditBinderLog = base44.entities.AuditBinderLog;
+
+export const InvoiceLineItem = base44.entities.InvoiceLineItem;
+
+export const PMActionLog = base44.entities.PMActionLog;
+
+export const CoordinatorDayPlan = base44.entities.CoordinatorDayPlan;
+
+export const CoordinatorDaySnapshot = base44.entities.CoordinatorDaySnapshot;
+
+export const DPRRun = base44.entities.DPRRun;
+
+export const CallActivity = base44.entities.CallActivity;
+
+export const MessageActivity = base44.entities.MessageActivity;
+
+export const PMScope = base44.entities.PMScope;
+
+export const PMEscalation = base44.entities.PMEscalation;
+
+export const Escalation = base44.entities.Escalation;
+
+export const EscalationContact = base44.entities.EscalationContact;
+
+export const EscalationMeetingRequest = base44.entities.EscalationMeetingRequest;
+
+export const EscalationTimeline = base44.entities.EscalationTimeline;
+
+export const EscalationNote = base44.entities.EscalationNote;
+
+export const IntroRequest = base44.entities.IntroRequest;
+
+export const JobThread = base44.entities.JobThread;
+
+export const ThreadMessage = base44.entities.ThreadMessage;
+
+export const AvailabilityBlock = base44.entities.AvailabilityBlock;
+
+export const Certificate = base44.entities.Certificate;
+
+export const JobAssignment = base44.entities.JobAssignment;
+
+export const MarketRate = base44.entities.MarketRate;
+
+export const RateEvaluation = base44.entities.RateEvaluation;
+
+export const ServiceAgreement = base44.entities.ServiceAgreement;
+
+export const Requisition = base44.entities.Requisition;
+
+export const FloatRequest = base44.entities.FloatRequest;
+
+export const Hold = base44.entities.Hold;
+
+export const CompanyConnection = base44.entities.CompanyConnection;
+
+export const CompanyOffice = base44.entities.CompanyOffice;
+
+export const CompanyContact = base44.entities.CompanyContact;
+
+export const CompanyDivision = base44.entities.CompanyDivision;
+
+export const CompanyRateCard = base44.entities.CompanyRateCard;
+
+export const ProductionCompany = base44.entities.ProductionCompany;
+
+export const ProductionLocation = base44.entities.ProductionLocation;
+
+export const EventsOutbox = base44.entities.EventsOutbox;
+
+export const CompanyKPICache = base44.entities.CompanyKPICache;
+
+export const SavedContact = base44.entities.SavedContact;
+
+export const NetworkingAuditLog = base44.entities.NetworkingAuditLog;
+
+export const UserTrackingAlert = base44.entities.UserTrackingAlert;
+
+export const TrackingAlert = base44.entities.TrackingAlert;
+
+export const CrewMeterScore = base44.entities.CrewMeterScore;
+
+export const ProjectPipeline = base44.entities.ProjectPipeline;
+
+export const FeatureFlag = base44.entities.FeatureFlag;
+
+export const AdvancedSearch = base44.entities.AdvancedSearch;
+
+export const MarketInsight = base44.entities.MarketInsight;
+
+export const JobBoard = base44.entities.JobBoard;
+
+export const JobApplication = base44.entities.JobApplication;
+
+export const VerifiedCredit = base44.entities.VerifiedCredit;
+
+export const OnboardingDocument = base44.entities.OnboardingDocument;
+
+export const IssueReport = base44.entities.IssueReport;
+
+export const CrewBadge = base44.entities.CrewBadge;
+
+export const OfferManagement = base44.entities.OfferManagement;
+
+export const CrewMediaAsset = base44.entities.CrewMediaAsset;
+
+export const CrewRepresentation = base44.entities.CrewRepresentation;
+
+export const PaymentRecord = base44.entities.PaymentRecord;
+
+export const TimesheetAutomation = base44.entities.TimesheetAutomation;
+
+export const ProductionSync = base44.entities.ProductionSync;
+
+export const LanguageTestSession = base44.entities.LanguageTestSession;
+
+export const LanguageTestAttempt = base44.entities.LanguageTestAttempt;
+
+export const SkillsTestAttempt = base44.entities.SkillsTestAttempt;
+
+export const SkillsTestCatalog = base44.entities.SkillsTestCatalog;
+
+export const GoldBadgeActivity = base44.entities.GoldBadgeActivity;
+
+export const TimesheetPerformance = base44.entities.TimesheetPerformance;
+
+export const GoldBadgeRevocation = base44.entities.GoldBadgeRevocation;
+
+export const TrainingOpportunity = base44.entities.TrainingOpportunity;
+
+export const IdempotencyKey = base44.entities.IdempotencyKey;
+
+export const TaxProfile = base44.entities.TaxProfile;
+
+export const ResidencyPermit = base44.entities.ResidencyPermit;
+
+export const TemporaryAccess = base44.entities.TemporaryAccess;
+
+export const AppAsset = base44.entities.AppAsset;
+
+export const PartnerLogo = base44.entities.PartnerLogo;
+
+export const I18nKey = base44.entities.I18nKey;
+
+export const I18nTranslation = base44.entities.I18nTranslation;
+
+export const I18nGlossary = base44.entities.I18nGlossary;
+
+export const I18nMemory = base44.entities.I18nMemory;
+
+export const I18nJob = base44.entities.I18nJob;
+
+export const I18nAudit = base44.entities.I18nAudit;
+
+export const I18nPackage = base44.entities.I18nPackage;
+
+export const I18nTranslateJob = base44.entities.I18nTranslateJob;
+
+export const I18nJobNote = base44.entities.I18nJobNote;
+
+export const I18nSettings = base44.entities.I18nSettings;
+
+export const AuthEvent = base44.entities.AuthEvent;
+
+export const CompanyPersonnel = base44.entities.CompanyPersonnel;
+
+export const PartnershipHistory = base44.entities.PartnershipHistory;
+
+export const CompanyFinancialProfile = base44.entities.CompanyFinancialProfile;
+
+export const IntelligenceAlert = base44.entities.IntelligenceAlert;
+
+export const EnterpriseClient = base44.entities.EnterpriseClient;
+
+export const ProductionProject = base44.entities.ProductionProject;
+
+export const TalentPipeline = base44.entities.TalentPipeline;
+
+export const TeamAssignment = base44.entities.TeamAssignment;
+
+export const FloatFinancing = base44.entities.FloatFinancing;
+
+export const ActivityLog = base44.entities.ActivityLog;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ functions.js
@@ -0,0 +1,449 @@
+import { base44 } from './base44Client';
+
+
+export const updateUserType = base44.functions.updateUserType;
+
+export const deepseekAI = base44.functions.deepseekAI;
+
+export const sendInviteEmail = base44.functions.sendInviteEmail;
+
+export const autoBookProjectDates = base44.functions.autoBookProjectDates;
+
+export const createUser = base44.functions.createUser;
+
+export const validateAccessToken = base44.functions.validateAccessToken;
+
+export const forceReAuthentication = base44.functions.forceReAuthentication;
+
+export const getEnrichedUsers = base44.functions.getEnrichedUsers;
+
+export const getUserStats = base44.functions.getUserStats;
+
+export const getAdminDashboardData = base44.functions.getAdminDashboardData;
+
+export const getCompanyDashboardData = base44.functions.getCompanyDashboardData;
+
+export const getCrewDashboardData = base44.functions.getCrewDashboardData;
+
+export const getCoordinatorHubStats = base44.functions.getCoordinatorHubStats;
+
+export const getCompanyProductions = base44.functions.getCompanyProductions;
+
+export const getVendorDashboardData = base44.functions.getVendorDashboardData;
+
+export const getCrewDatabase = base44.functions.getCrewDatabase;
+
+export const getRequestQueueData = base44.functions.getRequestQueueData;
+
+export const getFloatManagementData = base44.functions.getFloatManagementData;
+
+export const getCompanyRequests = base44.functions.getCompanyRequests;
+
+export const getMessageThreads = base44.functions.getMessageThreads;
+
+export const getThreadDetails = base44.functions.getThreadDetails;
+
+export const sendMessage = base44.functions.sendMessage;
+
+export const getUserManagementData = base44.functions.getUserManagementData;
+
+export const createStagedUser = base44.functions.createStagedUser;
+
+export const getAIMatchingData = base44.functions.getAIMatchingData;
+
+export const manageCallLogs = base44.functions.manageCallLogs;
+
+export const getRecentThreadsSummary = base44.functions.getRecentThreadsSummary;
+
+export const getCalendlyEvents = base44.functions.getCalendlyEvents;
+
+export const getNegotiationData = base44.functions.getNegotiationData;
+
+export const approveNegotiation = base44.functions.approveNegotiation;
+
+export const getActiveProjectStats = base44.functions.getActiveProjectStats;
+
+export const generateContract = base44.functions.generateContract;
+
+export const getAISuggestions = base44.functions.getAISuggestions;
+
+export const archiveProduction = base44.functions.archiveProduction;
+
+export const finalizeShortlist = base44.functions.finalizeShortlist;
+
+export const getCompanyTimesheetData = base44.functions.getCompanyTimesheetData;
+
+export const getTimesheetDetails = base44.functions.getTimesheetDetails;
+
+export const updateTimesheetStatus = base44.functions.updateTimesheetStatus;
+
+export const generateAuditBinder = base44.functions.generateAuditBinder;
+
+export const createSignedUrl = base44.functions.createSignedUrl;
+
+export const getFxRates = base44.functions.getFxRates;
+
+export const generateInvoices = base44.functions.generateInvoices;
+
+export const getCompanyFinancials = base44.functions.getCompanyFinancials;
+
+export const getContractData = base44.functions.getContractData;
+
+export const generateReport = base44.functions.generateReport;
+
+export const getProjectManagerStats = base44.functions.getProjectManagerStats;
+
+export const pmMocks = base44.functions.pmMocks;
+
+export const generatePMSeedData = base44.functions.generatePMSeedData;
+
+export const pmBulkActions = base44.functions.pmBulkActions;
+
+export const getRecommendations = base44.functions.getRecommendations;
+
+export const pmPrecheck = base44.functions.pmPrecheck;
+
+export const pmRequestActions = base44.functions.pmRequestActions;
+
+export const generateDPR = base44.functions.generateDPR;
+
+export const getPMScope = base44.functions.getPMScope;
+
+export const pmEscalate = base44.functions.pmEscalate;
+
+export const getRequestQueueDataEnterprise = base44.functions.getRequestQueueDataEnterprise;
+
+export const pmBulkActionsEnterprise = base44.functions.pmBulkActionsEnterprise;
+
+export const getProjectManagerStatsEnterprise = base44.functions.getProjectManagerStatsEnterprise;
+
+export const getTeamManagementData = base44.functions.getTeamManagementData;
+
+export const executeTeamRebalance = base44.functions.executeTeamRebalance;
+
+export const getProjectsManagementData = base44.functions.getProjectsManagementData;
+
+export const pmProjectActions = base44.functions.pmProjectActions;
+
+export const getActivityData = base44.functions.getActivityData;
+
+export const exportActivityData = base44.functions.exportActivityData;
+
+export const createEscalation = base44.functions.createEscalation;
+
+export const getEscalation = base44.functions.getEscalation;
+
+export const acknowledgeEscalation = base44.functions.acknowledgeEscalation;
+
+export const getCoordinatorAnalytics = base44.functions.getCoordinatorAnalytics;
+
+export const generateCoordinatorReport = base44.functions.generateCoordinatorReport;
+
+export const getCrewRosterData = base44.functions.getCrewRosterData;
+
+export const assignCrewMember = base44.functions.assignCrewMember;
+
+export const getContractTemplates = base44.functions.getContractTemplates;
+
+export const generateContractFromTemplate = base44.functions.generateContractFromTemplate;
+
+export const updateAvailabilityStatus = base44.functions.updateAvailabilityStatus;
+
+export const loadTestPMHub = base44.functions.loadTestPMHub;
+
+export const generateAccessibilityReport = base44.functions.generateAccessibilityReport;
+
+export const securityMiddleware = base44.functions.securityMiddleware;
+
+export const encryptSensitiveData = base44.functions.encryptSensitiveData;
+
+export const generateSecureEscalationLink = base44.functions.generateSecureEscalationLink;
+
+export const evaluateOffer = base44.functions.evaluateOffer;
+
+export const acceptOffer = base44.functions.acceptOffer;
+
+export const getProfileScore = base44.functions.getProfileScore;
+
+export const getNextBestAction = base44.functions.getNextBestAction;
+
+export const getAvailability = base44.functions.getAvailability;
+
+export const createAvailabilityBlock = base44.functions.createAvailabilityBlock;
+
+export const getRandomProfiles = base44.functions.getRandomProfiles;
+
+export const getAvailabilitySearch = base44.functions.getAvailabilitySearch;
+
+export const cleanupAvailabilityBlocks = base44.functions.cleanupAvailabilityBlocks;
+
+export const getRandomProfilesForAdmin = base44.functions.getRandomProfilesForAdmin;
+
+export const availabilityWebhook = base44.functions.availabilityWebhook;
+
+export const availabilityHealthCheck = base44.functions.availabilityHealthCheck;
+
+export const availabilityAnalytics = base44.functions.availabilityAnalytics;
+
+export const updateAvailabilityBlock = base44.functions.updateAvailabilityBlock;
+
+export const deleteAvailabilityBlock = base44.functions.deleteAvailabilityBlock;
+
+export const googleCalendarSync = base44.functions.googleCalendarSync;
+
+export const bulkAvailabilityOperations = base44.functions.bulkAvailabilityOperations;
+
+export const submitIntroductionRequest = base44.functions.submitIntroductionRequest;
+
+export const getIntroductionRequests = base44.functions.getIntroductionRequests;
+
+export const getCompanyAnalytics = base44.functions.getCompanyAnalytics;
+
+export const createRequisition = base44.functions.createRequisition;
+
+export const createFloatRequest = base44.functions.createFloatRequest;
+
+export const getCompanyProfile = base44.functions.getCompanyProfile;
+
+export const updateCompanyProfile = base44.functions.updateCompanyProfile;
+
+export const multiTenantDb = base44.functions.multiTenantDb;
+
+export const cacheManager = base44.functions.cacheManager;
+
+export const eventProcessor = base44.functions.eventProcessor;
+
+export const networkRolodex = base44.functions.networkRolodex;
+
+export const trackEntity = base44.functions.trackEntity;
+
+export const getUserAlerts = base44.functions.getUserAlerts;
+
+export const getProductionHubData = base44.functions.getProductionHubData;
+
+export const getCompanyActivity = base44.functions.getCompanyActivity;
+
+export const getCurrencyConfig = base44.functions.getCurrencyConfig;
+
+export const contractorClassificationEngine = base44.functions.contractorClassificationEngine;
+
+export const paymentScheduleProcessor = base44.functions.paymentScheduleProcessor;
+
+export const filmmakerProfileEnhanced = base44.functions.filmmakerProfileEnhanced;
+
+export const jobDiscoveryEngine = base44.functions.jobDiscoveryEngine;
+
+export const getCrewMeterScore = base44.functions.getCrewMeterScore;
+
+export const checkFeatureFlag = base44.functions.checkFeatureFlag;
+
+export const advancedSearch = base44.functions.advancedSearch;
+
+export const getMarketInsights = base44.functions.getMarketInsights;
+
+export const documentWorkflow = base44.functions.documentWorkflow;
+
+export const issueReporting = base44.functions.issueReporting;
+
+export const mediaManagement = base44.functions.mediaManagement;
+
+export const creditsManagement = base44.functions.creditsManagement;
+
+export const badgeSystem = base44.functions.badgeSystem;
+
+export const offerManagement = base44.functions.offerManagement;
+
+export const paymentProcessor = base44.functions.paymentProcessor;
+
+export const timesheetAutomation = base44.functions.timesheetAutomation;
+
+export const getFilmmakerProfileData = base44.functions.getFilmmakerProfileData;
+
+export const createFilmmakerCanonicalSchema = base44.functions.createFilmmakerCanonicalSchema;
+
+export const filmmakerCanonicalAPI = base44.functions.filmmakerCanonicalAPI;
+
+export const filmmakerEventsSchema = base44.functions.filmmakerEventsSchema;
+
+export const filmmakerCompatibilityFacade = base44.functions.filmmakerCompatibilityFacade;
+
+export const filmmakerSearchIndexer = base44.functions.filmmakerSearchIndexer;
+
+export const getFilmmakerEventSchemas = base44.functions.getFilmmakerEventSchemas;
+
+export const languageTestEngine = base44.functions.languageTestEngine;
+
+export const skillsTestEngine = base44.functions.skillsTestEngine;
+
+export const getSkillsTestCatalogSeed = base44.functions.getSkillsTestCatalogSeed;
+
+export const skillsTestBankValidator = base44.functions.skillsTestBankValidator;
+
+export const skillsTestPublisher = base44.functions.skillsTestPublisher;
+
+export const getFilmmakerBadges = base44.functions.getFilmmakerBadges;
+
+export const startSkillsTest = base44.functions.startSkillsTest;
+
+export const contractEventHandler = base44.functions.contractEventHandler;
+
+export const trackGoldBadgeActivity = base44.functions.trackGoldBadgeActivity;
+
+export const timesheetSubmissionTracker = base44.functions.timesheetSubmissionTracker;
+
+export const productionInfoManagement = base44.functions.productionInfoManagement;
+
+export const manageableEntityData = base44.functions.manageableEntityData;
+
+export const profileOrchestrator = base44.functions.profileOrchestrator;
+
+export const matchShortlist = base44.functions.matchShortlist;
+
+export const documentsFacade = base44.functions.documentsFacade;
+
+export const lib/healthUtils = base44.functions.lib/healthUtils;
+
+export const health = base44.functions.health;
+
+export const schemaCatalog = base44.functions.schemaCatalog;
+
+export const canonicalEnums = base44.functions.canonicalEnums;
+
+export const adminInviteUser = base44.functions.adminInviteUser;
+
+export const adminAssignRole = base44.functions.adminAssignRole;
+
+export const adminDisableUser = base44.functions.adminDisableUser;
+
+export const documentsPipeline = base44.functions.documentsPipeline;
+
+export const documentsExpiryAlerts = base44.functions.documentsExpiryAlerts;
+
+export const pdplOps = base44.functions.pdplOps;
+
+export const rateGuardrails = base44.functions.rateGuardrails;
+
+export const offersFacade = base44.functions.offersFacade;
+
+export const shortlistFacade = base44.functions.shortlistFacade;
+
+export const exportThreadPdf = base44.functions.exportThreadPdf;
+
+export const timesheetExportPdf = base44.functions.timesheetExportPdf;
+
+export const timesheetBatchApprove = base44.functions.timesheetBatchApprove;
+
+export const markThreadRead = base44.functions.markThreadRead;
+
+export const adminActivateUser = base44.functions.adminActivateUser;
+
+export const bulkDeleteRecords = base44.functions.bulkDeleteRecords;
+
+export const adminUpdateUserRecord = base44.functions.adminUpdateUserRecord;
+
+export const adminBanUser = base44.functions.adminBanUser;
+
+export const createTemporaryAccess = base44.functions.createTemporaryAccess;
+
+export const runContractTests = base44.functions.runContractTests;
+
+export const deadLinkCrawler = base44.functions.deadLinkCrawler;
+
+export const rtlA11yProbe = base44.functions.rtlA11yProbe;
+
+export const stagingSeed = base44.functions.stagingSeed;
+
+export const integrationsReport = base44.functions.integrationsReport;
+
+export const runPageSpecTests = base44.functions.runPageSpecTests;
+
+export const runCrawlerAndPerfProbes = base44.functions.runCrawlerAndPerfProbes;
+
+export const normalizeEnums = base44.functions.normalizeEnums;
+
+export const generateComplianceReport = base44.functions.generateComplianceReport;
+
+export const lib/securityHeaders = base44.functions.lib/securityHeaders;
+
+export const perfA11yProbes = base44.functions.perfA11yProbes;
+
+export const pageSpecRegistry = base44.functions.pageSpecRegistry;
+
+export const uiSession = base44.functions.uiSession;
+
+export const uiNavigation = base44.functions.uiNavigation;
+
+export const founderQuick = base44.functions.founderQuick;
+
+export const founderThresholds = base44.functions.founderThresholds;
+
+export const getAdminAlerts = base44.functions.getAdminAlerts;
+
+export const getAdminAlertCategories = base44.functions.getAdminAlertCategories;
+
+export const assignAlert = base44.functions.assignAlert;
+
+export const snoozeAlert = base44.functions.snoozeAlert;
+
+export const i18nApi = base44.functions.i18nApi;
+
+export const authTelemetry = base44.functions.authTelemetry;
+
+export const authPreflight = base44.functions.authPreflight;
+
+export const getEnvironmentConfig = base44.functions.getEnvironmentConfig;
+
+export const reconcilePlatformInvites = base44.functions.reconcilePlatformInvites;
+
+export const getAuthHealth = base44.functions.getAuthHealth;
+
+export const sendAuthReport = base44.functions.sendAuthReport;
+
+export const requestPlatformInvite = base44.functions.requestPlatformInvite;
+
+export const exportCompanyProfilePdf = base44.functions.exportCompanyProfilePdf;
+
+export const getCompanyNews = base44.functions.getCompanyNews;
+
+export const getActiveProjects = base44.functions.getActiveProjects;
+
+export const getAvailableTalent = base44.functions.getAvailableTalent;
+
+export const getPendingAssignments = base44.functions.getPendingAssignments;
+
+export const createProject = base44.functions.createProject;
+
+export const requestTeamAssembly = base44.functions.requestTeamAssembly;
+
+export const requestFloatFinancing = base44.functions.requestFloatFinancing;
+
+export const getEnterpriseMetrics = base44.functions.getEnterpriseMetrics;
+
+export const skillsTestsGetCatalog = base44.functions.skillsTestsGetCatalog;
+
+export const skillsTestsStart = base44.functions.skillsTestsStart;
+
+export const skillsTestsSubmit = base44.functions.skillsTestsSubmit;
+
+export const skillsTestsGetAttempts = base44.functions.skillsTestsGetAttempts;
+
+export const getPihKpiData = base44.functions.getPihKpiData;
+
+export const generateGovernmentKpiReport = base44.functions.generateGovernmentKpiReport;
+
+export const exportPihExcel = base44.functions.exportPihExcel;
+
+export const exportPihJson = base44.functions.exportPihJson;
+
+export const getPmHubData = base44.functions.getPmHubData;
+
+export const pmHubStaffingAction = base44.functions.pmHubStaffingAction;
+
+export const pmHubFloatAction = base44.functions.pmHubFloatAction;
+
+export const pmHubExceptionAction = base44.functions.pmHubExceptionAction;
+
+export const getPMHubData = base44.functions.getPMHubData;
+
+export const pmHubAction = base44.functions.pmHubAction;
+
